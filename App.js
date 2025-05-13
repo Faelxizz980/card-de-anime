fe2526d63d9fb76f9e74111e5fbd9cc449f4ee61
Faelxizz980 <shaolindoneu68@gmail.com>
2025-05-13 20:56:56 -0300

    Cards prontos

---
+++ App.js
@@ -0,0 +1,52 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import CardAnime from './components/cardAnime';
+import personagens from './data/personagens.json';
+import { ScrollView } from 'react-native-gesture-handler';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style ={styles.title}>Personagens</Text>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.row}>
+          {personagens.map((personagem) => (
+            <CardAnime
+              key={personagem.id}
+              nome={personagem.nome}
+              anime={personagem.anime}
+              imagem={personagem.imagem}
+            />
+          ))}
+        </View>
+      </ScrollView>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0a1536',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    flexWrap: 'wrap', // Permite que os cards quebrem linha se necessário
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 20,
+    marginTop: 50,
+  },
+});
---
+++ components/cardAnime.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+export default function CardAnime({ nome = "Não informado", anime = "Não informado", imagem }) {
+  return (
+    <View style={styles.card}>
+      <Image source={{ uri: imagem }} style={styles.image} />
+      <Text style={styles.nome}>{nome}</Text>
+      <Text style={styles.anime}>{anime}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    margin: 10,
+    padding: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+    flexDirection: 'column',
+    backgroundColor: '#08204d', // Cor padrão do card
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+  },
+  nome: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginTop: 10,
+  },
+  anime: {
+    fontSize: 14,
+    fontStyle: 'italic',
+    color: '#ffecec',
+  },
+});
